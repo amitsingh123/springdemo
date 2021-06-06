4c78ead934f349de91693321c8194f79f830eba3
Amit Singh <amit8829.jnnce@gmail.com>
2021-06-06 14:49:54 +0530

    custom AuthenticationProvider
    custom authentication prvider

--- springsecurity/src/main/java/com/amit/springsecurity/SpringSecurityApplication.java
+++ springsecurity/src/main/java/com/amit/springsecurity/SpringSecurityApplication.java
@@ -2,12 +2,17 @@ package com.amit.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
 
 @SpringBootApplication
 public class SpringSecurityApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
+		//To generate the hash from the password to store inside db for user password
+		// since bcrypt encoder is invalidating the plain text password
+		// will comment once create user api is done
+		String pw_hash = BCrypt.hashpw("12345", BCrypt.gensalt());
+		System.out.println("pw_hash...."+pw_hash);
 	}
-
 }
